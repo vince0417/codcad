c5b312fab3b47eb1338ac7423f5f96baa93bb123
vince0417 <vicenterodrigues56438@gmail.com>
2023-12-26 19:17:17 -0500

    Média Inteira

---
+++ programaçao basica/ex003/003.cpp
@@ -0,0 +1,14 @@
+//Média Inteira
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    int a, b, rstd;
+
+    cin >> a >> b;
+    rstd = (a+b)/2;
+
+    cout << rstd << endl;
+    return 0;
+}
